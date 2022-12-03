7e6257163f1380759bfc84e328063fb6e6216787
Zafar <umerov.z99@gmail.com>
2022-12-03 20:46:24 +0500

    Удаление лишнего кода

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:news_app/ui/app.dart';
 
 void main() {
-  // SystemChrome.setSystemUIOverlayStyle(
-  //   SystemUiOverlayStyle(
-  //     systemNavigationBarColor: const Color(0xff003366),
-  //   ),
-  // );
   runApp(const NewsApp());
 }
